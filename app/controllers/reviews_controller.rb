1e0a427859d20a6bbd2255756e3aa67e2a02a831
takuro1202 <66978685+takuro1202@users.noreply.github.com>
2020-11-17 16:36:29 +0900

    edit update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,6 +20,16 @@ class ReviewsController < ApplicationController
     review.destroy
   end
 
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    review = Review.find(params[:id])
+    review.update(review_params)
+  end
+
+
   private
   def review_params
     params.require(:review).permit(:cafename,:address,:image,:text,:evaluation1,:evaluation2,:evaluation3,:evaluation4,:evaluation5,:existence1,:existence2,:existence3).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "reviews#index"
   resources :users, only: [:edit, :update]
-  resources :reviews, only: [:index, :new, :create, :show, :destroy]
+  resources :reviews, only: [:index, :new, :create, :show, :destroy, :edit, :update]
 end
